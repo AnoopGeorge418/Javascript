5663534ba970708f3c9b0053467d23e37db3b55e
Anoop George <anoopgeorge418@gmail.com>
2025-11-06 23:09:30 +0530

    Login ui updated

--- 1. Expense Tracker/Js/login.js
+++ 1. Expense Tracker/Js/login.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const goBackBtn = document.querySelector(".goback");
+    const okBtn = document.getElementById("okBtn");
+
+    goBackBtn.addEventListener("click", () => {
+        window.location.href = "./index.html";
+    });
+
+    okBtn.addEventListener("click", () => {
+        // will be added
+    });
+
 })
